ad3fcdca19fa035ab74a1bb0ed1cf5fe54a7a0a5
Erintes <56921126+Erintes@users.noreply.github.com>
2020-12-15 18:19:46 +0500

    Update Exercises.scala

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,17 +1,32 @@
 object Exercises {
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
+  def fibonacci4Index(idx: Int): Int =
+    idx match {
+      case 0 => 0
+      case 1 | 2 => 1
+      case _ =>
+        var x = 1
+        var y = 1
+        var res = 0
+        for (i <- 2 until idx){
+          res = x + y
+          x = y
+          y = res
+        }
+        res
+    }
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
-
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
-
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] =
+    idx match {
+      case 0 => Seq(0)
+      case 1 => Seq(0, 1)
+      case _ =>
+        var res = Seq(0, 1)
+        for (i <- 1 until idx)
+          res :+= res.last + res.dropRight(1).last
+        res
+    }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +34,19 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = {
+    text
+      .map(sym => MORSE.getOrElse(sym.toString.toUpperCase, sym))
+      .mkString(" ")
+  }
 
-
-  def wordReverse(text: String): String = ???
-
+  def wordReverse(text: String): String = {
+    text.split("(?=[?!., ])|(?<=[?!., ])")
+      .map(word =>
+        if(word.charAt(0).isUpper)
+          word.toLowerCase.reverse.capitalize
+        else
+          word.reverse)
+      .mkString("")
+  }
 }
